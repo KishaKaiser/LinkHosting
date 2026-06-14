601dc72f38e3c6975ff05c133b86cff237c81217
KishaKaiser <lholder407@gmail.com>
2026-06-14 03:16:45 -0400

    Route PL_CMS first run to installer

--- control-plane/app/services/proxy.py
+++ control-plane/app/services/proxy.py
@@ -103,6 +103,10 @@ server {{
         proxy_set_header X-Forwarded-Proto $scheme;
     }}
 
+    location = / {{
+        return 302 /install;
+    }}
+
     location / {{
         proxy_pass http://{web_upstream}:3000;
         proxy_set_header Host $host;
@@ -167,6 +171,10 @@ server {{
         proxy_set_header X-Real-IP $remote_addr;
         proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
         proxy_set_header X-Forwarded-Proto $scheme;
+    }}
+
+    location = / {{
+        return 302 /install;
     }}
 
     location / {{
